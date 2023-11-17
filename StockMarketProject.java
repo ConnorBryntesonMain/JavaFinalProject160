b260eebbecadb4b7a2cadd05151afc3ef63a2b78
ConnorBryntesonMain <122413693+ConnorBryntesonMain@users.noreply.github.com>
2023-11-17 13:35:56 -0600

    Add files via upload

--- StockMarketProject.java
+++ StockMarketProject.java
@@ -3,7 +3,12 @@ import java.io.*;
 
 /**
  * Things to do
- * 1. Debug the array list to hold total bought stocks
+ * 1.
+ * Create a GUI that has a text box that displays arraylist of all the stocks
+ * and the prices, the stocks that the player bough,
+ * has a buy and sell, a text feild that displays total cash amount,
+ * and an editable text feild that accepts player input for how much they want
+ * to spend.
  * 2. Print out how they did to the output file
  * 3. Make an AI
  * 4. Run the caluclations in parelle
@@ -13,7 +18,6 @@ import java.io.*;
 public class StockMarketProject {
     public static double inOrOut = 0.0;
     public static int playerChoice;
-    public static double cash = 100000;
 
     public static void main(String[] args) throws FileNotFoundException {
         player myPlayer = new player("LLCTitle", 100, 0.1, "small", "spring");
--- player.java
+++ player.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class player extends Stock {
     private int count;
     private FlowerShares flowerShares;
     private double value;
+    public static double cash = 100000;
 
     public player(String LLCTitle, int startingPrice, double randomnessMod, String scale, String season) {
         super(LLCTitle, startingPrice, randomnessMod, scale, season);
@@ -23,13 +25,14 @@ public class player extends Stock {
         this.count = n;
     }
 
-    int totalTurns = 2;
+    int totalTurns = 5;
 
     void turn(Scanner input) {
+        action(input);
         while (totalTurns > 0) {
-            turnCounter(input);
             action(input);
-            FlowerShares.updatePrices();
+            turnCounter(input);
+            Stock.updatePrices();
             totalTurns--;
         }
     }
@@ -37,22 +40,16 @@ public class player extends Stock {
     int turnCount = 5;
 
     void turnCounter(Scanner input) {
-        turnCount = 5;
-        while (turnCount > 0) {
-
-            pickStock(input);
-
-            flowerShares.changeSeasonStock();
-            turnCount--;
-        }
+        flowerShares.changeSeasonStock();
+        turnCount--;
         setCount(0);
     }
 
     public ArrayList<Integer> arrayIndex = new ArrayList<>();
 
     public void pickStock(Scanner input) {
+        input.nextLine();
         int arrayCount = 0;
-
         for (Stock stocks : Stock.stockList) {
             if (count < 3) {
                 System.out.println(stocks);
@@ -80,43 +77,61 @@ public class player extends Stock {
         System.out.println("Would you like to buy or sell?");
         nextInput = input.next();
         if (nextInput.equalsIgnoreCase("buy")) {
-            for (int i = 0; i < arrayIndex.size(); i++) {
-                int num = arrayIndex.get(i);
-                System.out.println("How much would you like to spend on " + stockList.get(arrayIndex.get(i)));
-                double invest = input.nextInt();
-                if (invest > StockMarketProject.cash) {
-                    System.out.println(
-                            "Be aware you don't have enough cash on hand.\nYou have this much cash left "
-                                    + StockMarketProject.cash);
-                    System.out.println("How much would you like to spend on " + stockList.get(arrayIndex.get(i)));
-                    invest = input.nextInt();
-                }
-                StockMarketProject.cash -= invest;
-                double position = i + 0.0;
-                boughtStocks.add(position);
-                Stock bought = stockList.get(arrayIndex.get(i));
-                int price = bought.getPrice();
-                double scale = bought.getStockAmount();
-                boughtStocks.add(invest / price);
-                bought.setStockAmount(scale - (invest / price));
-
-            }
-            System.out.println(boughtStocks.toString());
+            pickStock(input);
+            buyAction(input);
         } else if (nextInput.equalsIgnoreCase("sell") && !boughtStocks.isEmpty()) {
-            for (int i = 0; i < arrayIndex.size(); i += 2) {
-                System.out.println("Would you like to sell" + arrayIndex.get(i - 1));
-                String yesorno = input.next();
-                if (yesorno.equalsIgnoreCase("yes")) {
-                    System.out.println("You have " + arrayIndex.get(i) + "left and the price is"
-                            + stockList.get(arrayIndex.get((5 * i) + 1)));
-                    System.out.println("How much do you want to sell");
-                    int nextNum = input.nextInt();
-                }
-
-            }
+            sellAction(input);
         } else {
             System.out.println("You can not sell when you have no stock.");
             action(input);
+        }
+    }
+
+    void buyAction(Scanner input) {
+        for (int i = 0; i < arrayIndex.size(); i++) {
+            int num = arrayIndex.get(i);
+            System.out.println("How much would you like to spend on " + stockList.get(arrayIndex.get(i)));
+            double invest = input.nextInt();
+            if (invest > cash) {
+                System.out.println(
+                        "Be aware you don't have enough cash on hand.\nYou have this much cash left "
+                                + cash);
+                System.out.println("How much would you like to spend on " + stockList.get(arrayIndex.get(i)));
+                invest = input.nextInt();
+            }
+            cash -= invest;
+            double position = i + 0.0;
+            boughtStocks.add(position);
+            Stock bought = stockList.get(arrayIndex.get(i));
+            int price = bought.getPrice();
+            double scale = bought.getStockAmount();
+            boughtStocks.add((invest * 1.0) / (price * 1.0));
+            bought.setStockAmount(scale - (invest / price));
+        }
+        System.out.println(boughtStocks.toString());
+    }
+
+    void sellAction(Scanner input) {
+        int stockCount = 1;
+        for (int i = 0; i < arrayIndex.size(); i++) {
+            System.out.println("You have " + boughtStocks.get(stockCount) + " left and the price is "
+                    + stockList.get(arrayIndex.get(i)));
+            System.out.println("How much do you want to sell");
+            double nextNum = input.nextDouble();
+
+            if (nextNum <= boughtStocks.get(stockCount)) {
+                Stock bought = stockList.get(arrayIndex.get(i));
+                double scale = bought.getStockAmount();
+                bought.setStockAmount(scale + nextNum);
+                cash += (nextNum * bought.getPrice());
+                if (nextNum == boughtStocks.get(stockCount)) {
+                    boughtStocks.remove(stockCount);
+                } else {
+                    double newStockAmount = boughtStocks.get(stockCount) - nextNum;
+                    boughtStocks.add(stockCount, newStockAmount);
+                }
+            }
+            stockCount += 2;
         }
     }
 }
