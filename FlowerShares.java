b3a16600815b9a63fefdfd934cb579253dfaad8c
ConnorBryntesonMain <122413693+ConnorBryntesonMain@users.noreply.github.com>
2023-11-29 17:50:48 -0600

    Add files via upload

--- FlowerShares.java
+++ FlowerShares.java
@@ -2,7 +2,7 @@ public class FlowerShares extends Stock {
     //This is the control if a stock has increased shares
     private int currentSeason;
     //This is the list of changes that can happen to a stock based on what season they are
-    private final int[] seasonChanges = { 50000, 10000, -10000, -50000 };
+    private final int[] seasonChanges = { 50000, 10000, -1000, -5000 };
     //This is where we control the season of when they increase and decrease
     public FlowerShares(String LLCTitle, int startingPrice, double randomnessMod, String scale, String season) {
         super(LLCTitle, startingPrice, randomnessMod, scale, season);
